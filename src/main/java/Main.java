1406abbc13cb3ac7cde66804273854c67e4f0c4f
Alex <alexius.lzc@gmail.com>
2024-08-14 13:54:53 +0800

    sample

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("started");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("waiting for input");
+        String s = scanner.nextLine();
+        System.out.println(s);
+    }
+}
